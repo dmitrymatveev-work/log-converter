bc367d0f7430570e00c678f72813e20932f21128
Dmitry Matveyev <dmitry.matveyev.job@gmail.com>
2018-06-26 13:30:42 +0300

    Implement getFiles function

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "log-converter/model"
+import (
+	"fmt"
+	"log-converter/model"
+	"os"
+	"strings"
+)
 
 func main() {
 	c := make(chan model.Entry, 10)
@@ -8,9 +13,9 @@ func main() {
 	for _, file := range getFiles() {
 		switch file.LogFormat {
 		case "first_format":
-			discoverFirst(c)
+			discoverFirst(file.FilePath, c)
 		case "second_format":
-			discoverSecond(c)
+			discoverSecond(file.FilePath, c)
 		}
 	}
 
@@ -20,15 +25,39 @@ func main() {
 }
 
 func getFiles() []model.File {
-	panic("the function is not implemented")
+	rawFiles := os.Args[1:]
+
+	if len(rawFiles) == 0 {
+		fmt.Println("Please provide file list.")
+	}
+
+	files := make([]model.File, 0)
+
+	for _, rawFile := range rawFiles {
+		rawMeta := strings.Split(rawFile, "|")
+
+		if len(rawMeta) != 2 {
+			fmt.Printf("Wrong argument format: \"%s\". Expected: \"file path|log format\".\n", rawFile)
+			continue
+		}
+
+		files = append(
+			files,
+			model.File{
+				FilePath:  strings.Trim(rawMeta[0], " \t"),
+				LogFormat: strings.Trim(rawMeta[1], " \t"),
+			})
+	}
+
+	return files
 }
 
-func discoverFirst(c chan<- model.Entry) {
-	panic("the function is not implemented")
+func discoverFirst(filePath string, c chan<- model.Entry) {
+	fmt.Printf("Discovering %s\n", filePath)
 }
 
-func discoverSecond(c chan<- model.Entry) {
-	panic("the function is not implemented")
+func discoverSecond(filePath string, c chan<- model.Entry) {
+	fmt.Printf("Discovering %s\n", filePath)
 }
 
 func store(e model.Entry) {
